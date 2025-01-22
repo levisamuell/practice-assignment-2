7bacdb32a4bce23e66451fd1a91d3bf092485c8e
levisamuel2005 <levi.samuel@kalvium.community>
2025-01-22 12:22:03 +0530

    added name and contact details

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return(
+        <div className='about'>
+            <h1>Levi</h1>
+            <p>Email: levi@gmail.com</p>
+            <p>Phone: 9611525125</p>
+        </div>
+    )
+}
+
+export default HomePage;
